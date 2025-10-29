0030d89113c6df43e4c48555ada1f0a7a948073d
DanilPOW <147328340+DanilPOW@users.noreply.github.com>
2025-10-29 18:45:47 +0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,15 @@
+import pytest
+import multiprocessing
+import time
+from app import iface  # Gradio интерфейс
 
+@pytest.fixture(scope="session")
+def start_gradio():
+    proc = multiprocessing.Process(
+        target=iface.launch,
+        kwargs={"server_name":"127.0.0.1","server_port":7860,"prevent_thread_lock": True}
+    )
+    proc.start()
+    time.sleep(5)  # ждём пока сервис стартанет
+    yield "http://127.0.0.1:7860"
+    proc.terminate()
